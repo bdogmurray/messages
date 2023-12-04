e01297418669b6349ba93f6ca31cb78cc8c9af84
bdogmurray <bdogmurray@aol.com>
2023-12-04 05:36:35 -0500

    revisions

--- Messages.js
+++ Messages.js
@@ -1,9 +1,12 @@
 // Generate Random messages  //
 
+let finalSentence = [];
+
+
 //  message part data  //
 const nouns = ["The guy ", "The gal ", "The zebra ", "The albatros ", "The elephant "];
 const verbs = ["ran quickly ", "stomped loudly ", "shuffled morosely ", "walked quietly ", "tip-toed gently "];
-const endings = ["to the bathroom.", "through the attic.", "under the ladder.", "between the flowers.", "from here to there."]
+const endings = ["to the bathroom.", "through the attic.", "under the ladder.", "between the flowers.", "from here to there."];
 
 //  generate random number 0-4 //
 
@@ -13,11 +16,15 @@ const getRandom = () => {
 
 //  pull random pieces of each part to make sentences  //
 
-cont makeSentence = () => {
-    finalSentence = nouns(getRandom) + verbs(getRandom) + endings(getRandom);
-    return finalSentence
+function makeSentence() {
+    finalSentence.push(nouns[getRandom]);
+    console.log(finalSentence)
+    finalSentence.push(verbs[getRandom]);
+    finalSentence.push(endings[getRandom]);
+    return finalSentence;
+    console.log(finalSentence);
 };
 makeSentence;
 
-console.log(makeSentence);
+// console.log(finalSentence);
 
