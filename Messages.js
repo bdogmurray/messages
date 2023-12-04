eccaea1ac380fd7f30ed041a23560d36a0bb2c6f
bdogmurray <bdogmurray@aol.com>
2023-12-03 20:59:22 -0500

    First attemt at program

--- Messages.js
+++ Messages.js
@@ -1,2 +1,23 @@
 // Generate Random messages  //
 
+//  message part data  //
+const nouns = ["The guy ", "The gal ", "The zebra ", "The albatros ", "The elephant "];
+const verbs = ["ran quickly ", "stomped loudly ", "shuffled morosely ", "walked quietly ", "tip-toed gently "];
+const endings = ["to the bathroom.", "through the attic.", "under the ladder.", "between the flowers.", "from here to there."]
+
+//  generate random number 0-4 //
+
+const getRandom = () => {
+    return Math.floor(Math.random() * 5);
+}
+
+//  pull random pieces of each part to make sentences  //
+
+cont makeSentence = () => {
+    finalSentence = nouns(getRandom) + verbs(getRandom) + endings(getRandom);
+    return finalSentence
+};
+makeSentence;
+
+console.log(makeSentence);
+
